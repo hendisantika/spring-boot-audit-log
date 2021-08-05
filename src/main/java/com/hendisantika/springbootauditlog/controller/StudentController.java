cd8c88a8b66ac0a2ec5d07466cbcfdeb2e011fa5
Hendi Santika <hendisantika@gmail.com>
2021-08-06 06:17:34 +0700

    #17 - Add list in StudentController

--- src/main/java/com/hendisantika/springbootauditlog/controller/StudentController.java
+++ src/main/java/com/hendisantika/springbootauditlog/controller/StudentController.java
@@ -1,8 +1,14 @@
 package com.hendisantika.springbootauditlog.controller;
 
+import com.hendisantika.springbootauditlog.dto.StudentDTO;
 import com.hendisantika.springbootauditlog.repository.StudentRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,12 +20,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Time: 06.15
  */
 @Controller
-@RequestMapping(value = "/student")
+@RequestMapping(value = "/students")
 public class StudentController {
 
     private final StudentRepository studentRepository;
 
     public StudentController(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        List<StudentDTO> studentDTOList = new ArrayList<>();
+        studentRepository.findAll().forEach(student -> {
+            studentDTOList.add(new StudentDTO(student));
+        });
+        model.addAttribute("studentDTOList", studentDTOList);
+        return "student/list";
     }
 }
